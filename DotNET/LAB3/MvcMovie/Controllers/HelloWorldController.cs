8e9fb414d79b9a1d6a7f7b61fc7a3ed82f1f3970
Alan Flisowski <flisowskialan@gmail.com>
2026-04-11 17:48:56 +0200

    LAB_3

--- DotNET/LAB3/MvcMovie/Controllers/HelloWorldController.cs
+++ DotNET/LAB3/MvcMovie/Controllers/HelloWorldController.cs
@@ -5,10 +5,14 @@ namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
+    // 
+    // GET: /HelloWorld/
     public IActionResult Index()
     {
         return View();
     }
+    // 
+    // GET: /HelloWorld/Welcome/ 
     public IActionResult Welcome(string name, int numTimes = 1)
     {
         ViewData["Message"] = "Hello " + name;
